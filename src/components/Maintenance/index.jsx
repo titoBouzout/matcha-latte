d63bbe51c46b1cb9f8139ed73354c567bff97778
Anthony K. <kilospade@gmail.com>
2025-11-09 23:02:16 -0600

    refactor maintenance

--- src/components/Maintenance/index.jsx
+++ src/components/Maintenance/index.jsx
@@ -3,69 +3,41 @@ import Card from "../Card";
 import styles from "../Dashboard/index.module.css";
 
 export default function Maintenance() {
-  async function runInNewTerm(cmd, envVars = {}) {
-    const envEntries = Object.entries(envVars);
-    const envString =
-      envEntries.length > 0
-        ? envEntries.reduce((a, c) => a + `${c[0]}=${c[1]}`, "") + " "
-        : "";
-    const args = ["-e", "fish", "-C", envString + cmd];
-    console.log(cmd, args);
-    Command.create("foot", args).execute();
-  }
+	async function runInNewTerm(cmd, envVars = {}) {
+		const envEntries = Object.entries(envVars);
+		const envString =
+			envEntries.length > 0
+				? envEntries.reduce((a, c) => a + `${c[0]}=${c[1]}`, "") + " "
+				: "";
+		const args = ["-e", "fish", "-C", envString + cmd];
+		console.log(cmd, args);
+		Command.create("foot", args).execute();
+	}
 
-  const commands = {
-    update: "ua-update-all",
-    yayCache: "yay -Sc",
-    pacmanCache: "sudo pacman -Sc",
-    orphans: "cleanup",
-    refreshMirrors: "ua-update",
-    btrfs: "",
-    removePacLock: "pac-unlock",
-    editRepos: "",
-    mergePacDiff: "pacdiff",
-  };
-  return (
-    <div class={styles.cards}>
-      <Card
-        onClick={() => {
-          runInNewTerm(commands.update);
-        }}
-      >
-        Update
-      </Card>
-      <Card
-        onClick={() => {
-          runInNewTerm(commands.pacmanCache);
-        }}
-      >
-        Clean pacman cache
-      </Card>
-      <Card
-        onClick={() => {
-          runInNewTerm(commands.yayCache);
-        }}
-      >
-        Clean yay cache
-      </Card>
-      <Card
-        onClick={() => {
-          runInNewTerm(commands.orphans);
-        }}
-      >
-        Clean orphans
-      </Card>
-      <Card>Refresh mirrors</Card>
-      <Card>Btrfs stuff</Card>
-      <Card>Remove pacman lock</Card>
-      <Card>Edit repos</Card>
-      <Card
-        onClick={() => {
-          runInNewTerm(commands.mergePacDiff, { DIFFPROG: "meld" });
-        }}
-      >
-        Merge pacdiff
-      </Card>
-    </div>
-  );
+	const commands = [
+		{ label: "Update", cmd: "ua-update-all" },
+		{ label: "Clean pacman cache", cmd: "sudo pacman -Sc" },
+		{ label: "Clean yay cache", cmd: "yay -Sc" },
+		{ label: "Clean orphans", cmd: "cleanup" },
+		{ label: "Refresh mirrors", cmd: "ua-update" },
+		{ label: "Btrfs stuff", cmd: "" },
+		{ label: "Remove pacman lock", cmd: "pac-unlock" },
+		{ label: "Edit repos", cmd: "" },
+		{ label: "Merge pacDiff", cmd: "pacdiff", env: { DIFFPROG: "meld" } },
+	];
+	return (
+		<div class={styles.cards}>
+			<For each={commands}>
+				{(item) => (
+					<Card
+						onClick={() => {
+							runInNewTerm(item.cmd, item.env);
+						}}
+					>
+						{item.label}
+					</Card>
+				)}
+			</For>
+		</div>
+	);
 }

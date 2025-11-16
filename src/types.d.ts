13ccf7f27a2b2c8658c43cafa66e9e08f9a5be38
Tito Bouzout <tito.bouzout@gmail.com>
2025-11-16 18:18:57 -0300

    add type for `use:tooltip`

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+import type { JSX } from "pota";
+
+declare module "pota" {
+	namespace JSX {
+		interface ElementAttributes<Element> {
+			"use:tooltip"?: {
+				position: { x: number; y: number };
+				value: Element;
+			};
+		}
+	}
+}
